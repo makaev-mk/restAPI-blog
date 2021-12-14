79e38a37950e68456cb4235fb646ee5ff68fb19c
makaew <makaev.mk@gmail.com>
2021-12-14 16:09:46 +0300

    change path for image

--- database/factories/ArticlesFactory.php
+++ database/factories/ArticlesFactory.php
@@ -16,7 +16,7 @@ class ArticlesFactory extends Factory
         return [
             'title' => $this->faker->text(20),
             'body' => $this->faker->text(200),
-            'image' => $this->faker->image('public', 640, 480, null, false),
+            'image' => $this->faker->image('public/storage', 640, 480, null, false),
             'likes' => $this->faker->numberBetween(0, 30),
             'views' => $this->faker->numberBetween(0, 50),
             'created_at' => $this->faker->dateTimeBetween('-3 month','now +1 month')
